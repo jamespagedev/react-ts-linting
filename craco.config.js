7959e265c67d9a58c9e4ce4fc35a760653cce46c
Vakon <jamespagedev@outlook.com>
2023-08-27 21:26:31 -0500

    initial setup

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@src': path.resolve(__dirname, './src')
+    }
+  }
+};
