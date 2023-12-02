de6bc12e908f9b5b36733cae2f1f6b43b85f8760
Vakon <jamespagedev@outlook.com>
2023-12-02 04:00:18 -0600

    test: add test type any

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Home } from 'src/views';
+import { sleep } from 'src/utils';
 
 function App() {
+  useEffect(() => {
+    const testTypeAny = async () => {
+      try {
+        await sleep(3000);
+      } catch (error: any) {
+        console.log('error:', error);
+      }
+    };
+    testTypeAny();
+  }, []);
   return <Home />;
 }
 
